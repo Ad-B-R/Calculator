1acae8d49b219003959524fb6accd87ba9a6f449
Adi B <186320933+Ad-B-R@users.noreply.github.com>
2025-07-27 17:17:23 +0000

    commit 5

--- script.js
+++ script.js
@@ -7,21 +7,24 @@ Calculator.addEventListener('click', function(event){
     
     const display = document.querySelector('#display');
     if(clickedKey.id=="="){
-        evaluate = display.value;
         display.value = eval(evaluate);
     }
     else if(clickedKey.id=="AC"){
         display.value = "";
         evaluate="";
     }
-    else if(clickedKey.id =="**"||clickedKey=="*"){
-
+    else if(clickedKey.id =="**"||clickedKey.id=="*"){
+        if(clickedKey.id=="**") display.value+="^"
+        else if(clickedKey.id=="*") display.value+=" x "
+        evaluate+=clickedKey.id;
     }
     else if (clickedKey.classList.contains("operations") && clickedKey.id!="(" && clickedKey.id!=")") 
     {
         display.value += " " + clickedKey.id + " ";
+        evaluate += " " + clickedKey.id + " ";
     }
     else{
         display.value += clickedKey.id;
+        evaluate+= clickedKey.id;
     }
 });
